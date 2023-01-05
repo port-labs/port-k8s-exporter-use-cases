c4b9c3049542ed809c1ae3ddd6a4a14213812cb0
Matan Heled <matanheled@getport.io>
2023-01-05 16:10:53 +0200

    Fixed argo use-case

--- argocd/blueprints_tf/argocdpapp.tf
+++ argocd/blueprints_tf/argocdpapp.tf
@@ -3,20 +3,41 @@ resource "port-labs_blueprint" "argocdApp" {
   icon       = "Service"
   identifier = "argocdApp"
   properties {
+    identifier = "status"
+    type = "string"
+  }
+  properties {
+    identifier = "syncStatus"
+    type = "string"
+  }
+  properties {
+    identifier = "gitRepo"
+    type = "string"
+    format = "url"
+  }
+  properties {
+    identifier = "gitPath"
+    type = "string"
+  }
+  properties {
+    identifier = "gitRev"
+    type = "string"
+  }
+  relations {
     identifier = "services"
     type       = "string"
     title      = "Services"
     required   = true
     many       = true
   }
-  properties {
+  relations {
     identifier = "deployments"
     type       = "string"
     title      = "Deployments"
     required   = true
     many       = true
   }
-  properties {
+  relations {
     identifier = "project"
     type       = "string"
     title      = "Project"
